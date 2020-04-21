37726385133069d9686d8a50677de2ba52ac4d4f
fernandogatto <fernandogatto17@gmail.com>
2020-04-21 20:11:18 -0300

    First commit

---
+++ src/routes/transactions.routes.ts
@@ -0,0 +1,47 @@
+import { Router } from 'express';
+
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+// import DeleteTransactionService from '../services/DeleteTransactionService';
+// import ImportTransactionsService from '../services/ImportTransactionsService';
+
+const transactionsRouter = Router();
+
+transactionsRouter.get('/', async (request, response) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRepository.find();
+
+  return response.json(transactions);
+});
+
+transactionsRouter.post('/', async (request, response) => {
+  try {
+    const { title, value, type, category } = request.body;
+
+    const createTransaction = new CreateTransactionService();
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    console.log(transaction);
+
+    return response.json(transaction);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+// transactionsRouter.delete('/:id', async (request, response) => {
+//   // TODO
+// });
+
+// transactionsRouter.post('/import', async (request, response) => {
+//   // TODO
+// });
+
+export default transactionsRouter;
---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,66 @@
+import { getRepository, getCustomRepository } from 'typeorm';
+
+// import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
+class CreateTransactionService {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('This transaction must be income or outcome.');
+    }
+
+    const balance = await transactionsRepository.getBalance();
+
+    if (type === 'outcome') {
+      const { total } = balance;
+
+      if (value > total) {
+        throw Error('This transaction has a outcome bigger than total.');
+      }
+    }
+
+    let categoryTransaction = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryTransaction) {
+      categoryTransaction = await categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(categoryTransaction);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryTransaction.id,
+    });
+
+    await categoriesRepository.save(transaction);
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
